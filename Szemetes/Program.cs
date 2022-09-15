662aa6199bf03791fbe07f0b919e2e419942ff92
D430RO <D430RO@I417-02>
2022-09-15 09:27:49 +0200

    Add project files.

---
+++ Szemetes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Szemetes
+{
+    class Szamolo
+    {
+        public static int number = 0;
+
+        public Szamolo() 
+        {
+            number++;
+            Console.WriteLine(number);
+        }
+
+        ~Szamolo() 
+        {
+            number = 0;
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            while (true) 
+            {
+                Szamolo sz = new Szamolo();
+
+                if (Szamolo.number == 0){
+                    Console.WriteLine("Vegeztem");
+                    break;
+                }
+            }
+
+
+
+        }
+    }
+}
